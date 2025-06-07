54dbb5c77c4a78dfbcfbf6125815e262b7b0e298
hu0810 <46022463+hu0810@users.noreply.github.com>
2025-06-07 23:27:38 +0800

    refactor: reorganize project structure

--- scripts/export-settings.sh
+++ scripts/export-settings.sh
@@ -5,7 +5,7 @@ APISIX_ADMIN_URL=${APISIX_ADMIN_URL:-http://localhost:9180/apisix/admin}
 APISIX_ADMIN_API_KEY=${APISIX_ADMIN_API_KEY:-admin123}
 
 for item in routes services upstreams consumers ssl global_rules; do
-  curl -s "$APISIX_ADMIN_URL/$item" -H "X-API-KEY: $APISIX_ADMIN_API_KEY" > ./raw_settings/${item}.json
+  curl -s "$APISIX_ADMIN_URL/$item" -H "X-API-KEY: $APISIX_ADMIN_API_KEY" > ./config/raw/${item}.json
 done
 
 #when etcd_data cannot be loaded
